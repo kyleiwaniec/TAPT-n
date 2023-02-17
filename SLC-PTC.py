71e3dfe44fae9863b560c924fe27fb2994ba8312
kyle Hamilton <kylehamilton@MacBook-Pro-4.local>
2023-02-17 21:02:37 +0000

    .

--- SLC-PTC.py
+++ SLC-PTC.py
@@ -51,8 +51,8 @@ training_args = TrainingArguments(
     output_dir=chkp+"_tiny_PTC/",
     evaluation_strategy="epoch",
     learning_rate=2e-5,
-    per_device_train_batch_size=48,
-    per_device_eval_batch_size=48,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
     num_train_epochs=3,
     weight_decay=0.01
 )
