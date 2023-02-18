407f3f4d847d06bbbfdf013279a59239272072ad
kyle Hamilton <kylehamilton@MacBook-Pro-4.local>
2023-02-18 17:44:44 +0000

    .

--- eval.py
+++ eval.py
@@ -21,7 +21,7 @@ pipe = pipeline("text-classification",
                       num_workers=160)
 
 predictions_binary=[]
-t = tqdm(pipe(dataset))
+t = tqdm(pipe(dataset()))
 for i in t:
     t.set_description("processing:", refresh=True)
     print(i)
