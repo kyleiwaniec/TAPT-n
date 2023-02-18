cef9533b47b16154d7be7586bf9171a45cdac523
kyle Hamilton <kylehamilton@MacBook-Pro-4.local>
2023-02-18 18:04:32 +0000

    .

--- eval.py
+++ eval.py
@@ -21,15 +21,14 @@ pipe = pipeline("text-classification",
                       num_workers=160)
 
 predictions_binary=[]
-t = tqdm(pipe(KeyPairDataset(dataset, "text","labels")))
+t = tqdm(dataset)
 for i in t:
     t.set_description("processing:", refresh=True)
-    print(i)
-#     pred = pipe(i['text']) # 'LABEL_0'
-#     y_hat = int(pred[0]['label'].split('_')[1])
-#     score = pred[0]['score'] if y_hat == 1 else 1-pred[0]['score']
-#     y = i['labels']
-#     predictions_binary.append([y_hat,y,score])
+    pred = pipe(i['text']) # 'LABEL_0'
+    y_hat = int(pred[0]['label'].split('_')[1])
+    score = pred[0]['score'] if y_hat == 1 else 1-pred[0]['score']
+    y = i['labels']
+    predictions_binary.append([y_hat,y,score])
     
 print(len(predictions_binary))
 
@@ -38,19 +37,3 @@ print(len(predictions_binary))
 
 print(classification_report(y_true_b, y_pred_b))
 
-
-
-# import datasets
-# from transformers import pipeline
-# from transformers.pipelines.pt_utils import KeyDataset
-# from tqdm.auto import tqdm
-
-# pipe = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-base-960h", device=0)
-# dataset = datasets.load_dataset("superb", name="asr", split="test")
-
-# # KeyDataset (only *pt*) will simply return the item in the dict returned by the dataset item
-# # as we're not interested in the *target* part of the dataset. For sentence pair use KeyPairDataset
-# for out in tqdm(pipe(KeyDataset(dataset, "text"))):
-#     print(out)
-#     # [{'label': 'LABEL_0', 'score': 0.9980136156082153}]
-#     # ....
