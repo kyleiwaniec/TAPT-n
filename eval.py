ae3df3fd2f27e170a165b3418f200749004586c2
kyle Hamilton <kylehamilton@MacBook-Pro-4.local>
2023-02-19 17:56:27 +0000

    .

--- eval.py
+++ eval.py
@@ -10,7 +10,7 @@ dataset = load_dataset('Kyleiwaniec/PTC_Corpus',
                        split="test",
                        use_auth_token='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')
 
-chkp = "../models/PTC_TAPT_n_RoBERTa_SLC_PTC_e9/"
+chkp = "../models/TAPT_n_RoBERTa_TC_PTC/"
 
 tokenizer = AutoTokenizer.from_pretrained(chkp)
 pipe = pipeline("text-classification", 
@@ -20,20 +20,21 @@ pipe = pipeline("text-classification",
                       device=0,
                       num_workers=160)
 
-predictions_binary=[]
+predictions=[]
 t = tqdm(dataset)
 for i in t:
     t.set_description("processing:", refresh=True)
     pred = pipe(i['text']) # 'LABEL_0'
     y_hat = int(pred[0]['label'].split('_')[1])
-    score = pred[0]['score'] if y_hat == 1 else 1-pred[0]['score']
+#     score = pred[0]['score'] if y_hat == 1 else 1-pred[0]['score']
     y = i['labels']
-    predictions_binary.append([y_hat,y,score])
+#     predictions_binary.append([y_hat,y,score])
+    predictions.append([y_hat,y]
     
-print(len(predictions_binary))
+print(len(predictions))
 
-y_true_b = np.array(predictions_binary)[:,1]
-y_pred_b = np.array(predictions_binary)[:,0]
+y_true_b = np.array(predictions)[:,1]
+y_pred_b = np.array(predictions)[:,0]
 
 print(classification_report(y_true_b, y_pred_b))
 
